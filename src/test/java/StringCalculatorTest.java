4a4f82a78d4900530a5e081de7872fbd29f0446c
Nikhil-Umesh <nikhil_u123@hotmail.com>
2021-09-11 22:01:14 +0530

    Complete Kata

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,45 +1,61 @@
-import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
-    private StringCalculator calculator;
-    @Before
-    public void initialize() {
-        calculator = new StringCalculator();
+    @Test
+    public void
+    return_0_when_input_is_empty() {
+        assertEquals("0", given(""));
     }
 
     @Test
-    public void testEmptyStringShouldReturn0() {
-        assertEquals(calculator.add(""), 0);
+    public void
+    return_3_when_input_is_1_2() {
+        assertEquals("3", given("1,2"));
     }
 
     @Test
-    public void testSingleNumberStringShouldReturnSameNumber() {
-        assertEquals(calculator.add("1"), 1);
-        assertEquals(calculator.add("2"), 2);
-        assertEquals(calculator.add("76"), 76);
+    public void
+    sum_floats_and_return_float() {
+        assertEquals("6.6", given("2.2,4.4"));
     }
 
     @Test
-    public void testFor2Numbers() {
-        assertEquals(calculator.add("1,2"), 3);
-        assertEquals(calculator.add("71,33"), 104);
-        assertEquals(calculator.add("103,104"),207);
+    public void
+    treat_newLine_as_a_delimiter() {
+        assertEquals("6", given("1\n2,3"));
     }
 
     @Test
-    public void testForUnknownNumberOfInputs() {
-        assertEquals(calculator.add("1,2"), 3);
-        assertEquals(calculator.add("71,33,1,103"), 208);
-        assertEquals(calculator.add("103,104,11,12,5,7,1,12,99"),354);
+    public void
+    return_error_msg_when_newLine_at_invalid_position() {
+        assertEquals("Number expected but '\n' found at position 6.", given("1,2,5,\n3"));
     }
 
     @Test
-    public void numbersNewlineDelimitedShouldBeSummed() {
-        assertEquals(calculator.add("1\n2"), 3);
-        assertEquals(calculator.add("11\n13"), 24);
-        assertEquals(calculator.add("1,2\n3"), 6);
+    public void
+    return_error_msg_when_delimiter_at_last_position() {
+        assertEquals("Number expected but EOF found.", given("2,3,4.2,"));
+    }
+
+    @Test
+    public void
+    return_correct_sum_when_custom_delimiter_is_used() {
+        assertEquals("3", given("//;\n1;2"));
+        assertEquals("3", given("//|\n1|2"));
+        assertEquals("8", given("//@@\n1@@2@@5"));
+        assertEquals("5", given("//sep\n2sep3"));
+    }
+
+    @Test
+    public void
+    return_string_of_negative_numbers_when_negative_numbers_are_used_as_input() {
+        assertEquals("Negative not allowed: -1", given("-1,2"));
+        assertEquals("Negative not allowed: -4,-5", given("2,-4,-5"));
+    }
+
+    private String given(String number) {
+        StringCalculator stringCalculator = new StringCalculator();
+        return stringCalculator.sum(number);
     }
 }
